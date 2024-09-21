57801c22b2840397ed1e970370a7be5757c73206
stas0o0 <stanislaw200917@gmail.com>
2024-09-21 13:02:11 +0300

    Task 37847 is done

--- 16/37847.py
+++ 16/37847.py
@@ -2,21 +2,21 @@ max1 = -30001
 max2 = -30001
 min1 = 30001
 min2 = 30001
-number = int(input())
-while number != 0:
-    if number > max1:
+numbers = int(input())
+while numbers != 0:
+    if numbers > max1:
         max2 = max1
-        max1 = number
-    elif number > max2:
-        max2 = number
+        max1 = numbers
+    elif numbers > max2:
+        max2 = numbers
 
-    if number < min1:
+    if numbers < min1:
         min2 = min1
-        min1 = number
-    elif number < min2:
-        min2 = number
+        min1 = numbers
+    elif numbers < min2:
+        min2 = numbers
 
-    number = int(input())
+    numbers = int(input())
 
 print(max1 + max2)
 print(min1 + min2)
